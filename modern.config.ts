357d11ac79c29a453c46b982a83863f064b90c75
gjssss <gaojinsong2010@gmail.com>
2025-05-31 17:40:08 +0000

    feat: init repo

--- modern.config.ts
+++ modern.config.ts
@@ -1,4 +1,6 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
+import { bffPlugin } from '@modern-js/plugin-bff';
+import { tailwindcssPlugin } from '@modern-js/plugin-tailwindcss';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
@@ -9,5 +11,7 @@ export default defineConfig({
     appTools({
       bundler: 'rspack', // Set to 'webpack' to enable webpack
     }),
+    bffPlugin(),
+    tailwindcssPlugin(),
   ],
 });
